7d34899c25317c6d12e70c871097058fbc7fa14e
Pape Momar Ndiaye <pmomar44@gmail.com>
2021-02-23 15:01:13 +0000

    Creation Of Tableaux Shower

--- src/main/Tableaux.js
+++ src/main/Tableaux.js
@@ -5,72 +5,128 @@ import image1 from "./Images/eE4cYLEA.jpeg"
 import image2 from "./Images/IMG_20180214_105003_edit.jpg"
 import image3 from "./Images/IMG_20180214_105555_edit.jpg"
 
-// let TableauxArray = [
-//     {
-//         image: './Images/1590098684498-2042443623[21808].jpg', title: 'title1', description: 'description1',
-//         image: './Images/1590098684498-2042443623[21808].jpg', title: 'title2', description: 'description2',
-//         image: './Images/1590098684498-2042443623[21808].jpg', title: 'title3', description: 'description3',
-//         image: './Images/1590098684498-2042443623[21808].jpg', title: 'title4', description: 'description4',
-//         image: './Images/1590098684498-2042443623[21808].jpg', title: 'title5', description: 'description5',
-//         image: './Images/1590098684498-2042443623[21808].jpg', title: 'title6', description: 'description6',
-//     },
-// ]
+let TableauxArray1 = [
+    { image: image3, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title2', description: 'description1' },
+    { image: image1, title: 'title1', description: 'description1' },
+    { image: image0, title: 'title1', description: 'description1' },
+    { image: image3, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' }
+]
+let TableauxArray2 = [
+    { image: image2, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' },
+    { image: image0, title: 'title1', description: 'description1' },
+    { image: image3, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' }
+]
+let TableauxArray3 = [
+    { image: image1, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' },
+    { image: image0, title: 'title1', description: 'description1' },
+    { image: image3, title: 'title1', description: 'description1' },
+    { image: image2, title: 'title1', description: 'description1' }
+]
 
 class Tableaux extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            TheTableauxToShow: TableauxArray1[0].image,
+            TheTitleToShow: TableauxArray1[0].title,
+            TheDescriptionToShow: TableauxArray1[0].description,
+            AllTableauxInRow1: [],
+            AllTableauxInRow2: [],
+            AllTableauxInRow3: [],
+        }
+        this.handelClose = this.handelClose.bind(this)
+        this.sowOneTableau = this.sowOneTableau.bind(this)
+    }
+
+    componentDidMount() {
+        let ArrayContainer1 = [];
+        let ArrayContainer2 = [];
+        let ArrayContainer3 = [];
+
+        TableauxArray1.map((tableau) => {
+            ArrayContainer1.push(<OneTableaux
+                onSowOneTableau={this.sowOneTableau}
+                TableauxUrl={tableau.image}
+                Title={tableau.title}
+                Description={tableau.description}
+            />)
+        })
+        this.setState({
+            AllTableauxInRow1: ArrayContainer1,
+        })
+        // ########################################
+        TableauxArray2.map((tableau) => {
+            ArrayContainer2.push(<OneTableaux
+                onSowOneTableau={this.sowOneTableau}
+                TableauxUrl={tableau.image}
+                Title={tableau.title}
+                Description={tableau.description} />)
+        })
+        this.setState({
+            AllTableauxInRow2: ArrayContainer2,
+        })
+        // ########################################
+        TableauxArray3.map((tableau) => {
+            ArrayContainer3.push(<OneTableaux
+                onSowOneTableau={this.sowOneTableau}
+                TableauxUrl={tableau.image}
+                Title={tableau.title}
+                Description={tableau.description} />)
+        })
+        this.setState({
+            AllTableauxInRow3: ArrayContainer3,
+        })
+    }
+
+    handelClose() {
+        document.querySelector(".tableaux-presentation-container").style.display = "none";
+    }
+
+    sowOneTableau(childData) {
+        this.setState({
+            TheTableauxToShow: childData.TableauxUrl,
+            TheDescriptionToShow: childData.Description,
+            TheTitleToShow: childData.Title
+        })
+        document.querySelector(".tableaux-presentation-container").style.display = "flex";
+    }
 
     render() {
         return (
             <Fragment>
                 <div id="tableaux-container">
                     <div className="tableaux-presentation-container">
-                        <div className="close-the-tableaux">
+                        <div className="close-the-tableaux"
+                            onClick={this.handelClose}
+                        >
                         </div>
                         <div className="the-tableaux">
-                            <h2>title</h2>
-                            <img src={image3} />
+                            <h2> {this.state.TheTitleToShow}</h2>
+                            <img src={this.state.TheTableauxToShow} />
                             <div className="the-tableaux-description">
-
                                 <p>
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                    Natus corporis delectus
-                                    blanditiis porro perferendis tempore nam deleniti
-                                    reprehenderit accusamus quia. Provident, consequuntur eius
-                                    aliquam rem aut sunt dignissimos. Alias, ullam.
-                            </p>
+                                    {this.state.TheDescriptionToShow}
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="tableaux-grid">
                         <div className="tableaux-rows">
-                            <OneTableaux TableauxUrl={image0} />
-                            <OneTableaux TableauxUrl={image1} />
-                            <OneTableaux TableauxUrl={image0} />
-                            <OneTableaux TableauxUrl={image1} />
-                            <OneTableaux TableauxUrl={image0} />
-                            <OneTableaux TableauxUrl={image1} />
-                            <OneTableaux TableauxUrl={image0} />
-                            <OneTableaux TableauxUrl={image1} />
+                            {this.state.AllTableauxInRow1}
                         </div>
                         <div className="tableaux-rows">
-                            <OneTableaux TableauxUrl={image1} />
-                            <OneTableaux TableauxUrl={image0} />
-                            <OneTableaux TableauxUrl={image1} />
-                            <OneTableaux TableauxUrl={image0} />
-                            <OneTableaux TableauxUrl={image1} />
-                            <OneTableaux TableauxUrl={image0} />
+                            {this.state.AllTableauxInRow2}
                         </div>
                         <div className="tableaux-rows">
-                            <OneTableaux TableauxUrl={image3} />
-                            <OneTableaux TableauxUrl={image2} />
-                            <OneTableaux TableauxUrl={image3} />
-                            <OneTableaux TableauxUrl={image2} />
-                            <OneTableaux TableauxUrl={image3} />
-                            <OneTableaux TableauxUrl={image2} />
-                            <OneTableaux TableauxUrl={image3} />
-                            <OneTableaux TableauxUrl={image2} />
-                            <OneTableaux TableauxUrl={image3} />
-                            <OneTableaux TableauxUrl={image2} />
+                            {this.state.AllTableauxInRow3}
                         </div>
                     </div>
 
@@ -81,20 +137,29 @@ class Tableaux extends Component {
 }
 
 
-
-
 class OneTableaux extends Component {
     constructor(props) {
         super(props);
+        this.handelSow = this.handelSow.bind(this)
+    }
+    handelSow() {
+        this.props.onSowOneTableau({
+            TableauxUrl: this.props.TableauxUrl,
+            Description: this.props.Description,
+            Title: this.props.Title
+        })
     }
 
     render() {
         return (
-            <div className="tableaux-container">
-                <h3>Titre</h3>
+            <div className="tableaux-container"
+                onClick={this.handelSow}>
+                <h3>
+                    {this.props.Title}
+                </h3>
                 <img src={this.props.TableauxUrl} alt="" width="100%" />
                 <p className="tableaux-description">
-                    description sadad
+                    {this.props.Description}
                 </p>
             </div>
         )
